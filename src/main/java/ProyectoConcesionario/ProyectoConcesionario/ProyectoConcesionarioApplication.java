a38cf7b7e7021c2420ea81a86f5b8711b48a6960
Jorstev <71983628+Jorstev@users.noreply.github.com>
2022-10-24 16:46:26 -0600

    Initial commit with Landing Page
    Initial commit with Landing Page

---
+++ src/main/java/ProyectoConcesionario/ProyectoConcesionario/ProyectoConcesionarioApplication.java
@@ -0,0 +1,13 @@
+package ProyectoConcesionario.ProyectoConcesionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoConcesionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoConcesionarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProyectoConcesionario/ProyectoConcesionario/ProyectoConcesionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoConcesionario.ProyectoConcesionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoConcesionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
